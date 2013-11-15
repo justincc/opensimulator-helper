db5a494c5e0b2937498169b08e016c5c66f1136e
Justin Clark-Casey <justincc@justincc.org>
2013-11-15 01:46:43 +0000

    Initial landtool.php obtained from http://pastebin.com/atAmukt2.  Also added instructions on use.

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ opensimulator-helper
 ====================
 
 Very small repository for containing PHP helper code necessary for some transactions involving an economy (e.g. selling land even for $0)
+
+Code originally comes from Melanie Theilker and Teravus Ovares with some cleanup by BlueWall.
+
+I will maintain this code but have no current plans to extend it (i.e. I won't develop it into a proper economy module unless someone heavily incentivises it :)
+
+However, patches are welcome.
+
+Background
+==========
+
+In the Second Life system (and hence in OpenSimulator), land sales, even for $0, require the viewer to directly interact with a set of 
+XMLRPC functions.  The landtool.php in this repository supplies a minimum version of these functions, so that sales can be made for $0.  Sales
+for any other amount will probably also work but no actual money transactions will occur (i.e. the sales will still be for free).
+
+Instructions
+============
+
+1) Configure landtool.php with a mysql user that can access your OpenSimulator ROBUST databases.  This is required so that the code can
+authenticate that requests are coming from valid logged in users.
+
+2) Place landtool.php in a place where it can be accessed via a web-server.  
+You will also need PHP to be active with the xmlrpc extension (in Ubuntu this is the package named php5-xmlrpc).
+
+3) Edit the economy parameter in the [GridInfo] section in your bin/Robust.ini OpenSimulator file (or Robust.HG.ini if appropriate) so that it points
+to landtool.php.  For instance, if landtool.php is accessible via the URL http://example.com/landtool.php, then you will need to configure
+
+[GridInfo]
+economy = http://example.com/landtool.php
+
+Since the viewer is contact this URL directly, it must be accessible to anybody who logs in to your grid.
+
+4) Restart the ROBUST instance hosting the login service.  Users that have previously logged in to your grid with a viewer that records grid URLs
+may need to refresh this information.
+
+A user should now be able to buy land.  Please note that on some viewers (e.g. Singularity 1.8.3) you may need to enable the admin menu
+(shortcut ctrl+alt+v) before you can buy land, even though you do not need to have admin status.  This may be a viewer bug or OpenSimulator is
+not currently supplying the correct information to enable land sales properly.
---
+++ landtool.php
@@ -0,0 +1,145 @@
+<?php
+# Copyright (c)Melanie Thielker and Teravus Ovares (http://opensimulator.org/)
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#      * Neither the name of the OpenSim Project nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+#  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# updated for Robust installations: BlueWall 2011
+
+
+  #
+  # Settings
+  #
+  $dbhost = "127.0.0.1";
+  $dbname = "opensimmaster";
+  $dbuser = "root";
+  $dbpass = "passw0rd";
+  # 
+  # Set this to your economy source / sink accounts
+  $economy_source_account = "3d6181b0-6a4b-97ef-18d8-722652995cf1";
+  $economy_sink_account="3d6181b0-6a4b-97ef-18d8-722652995cf1";
+
+  # Tables
+  $presence = "Presence";
+
+  $xmlrpc_server = xmlrpc_server_create();
+
+  function process_transaction($avatarId, $amount, $ipAddress)
+  {
+    # Do Credit Card Processing here!  Return False if it fails!
+    # Remember, $amount is stored without decimal places, however it's assumed
+    # that the transaction amount is in Cents and has two decimal places
+    # 5 dollars will be 500
+    # 15 dollars will be 1500
+
+    return True;
+  }
+
+  function validate_user($agent_id, $s_session_id)
+  {
+    global $dbhost, $dbuser, $dbpass, $dbname;
+
+    $agentid = mysql_escape_string($agent_id);
+    $sessionid = mysql_escape_string($s_session_id);
+
+    $link = mysql_connect($dbhost, $dbuser, $dbpass)
+      or die('ERROR: '.mysql_error());
+
+    mysql_select_db($dbname);
+
+    $query = "select UserID from Presence where UserID='".$agentid."' and SecureSessionID = '".$sessionid."'";
+
+    $result = mysql_query($query)
+      or die('ERROR: '.mysql_error());
+
+    $row = mysql_fetch_assoc($result);
+
+    return $row['UserID'];
+  }
+
+
+  xmlrpc_server_register_method($xmlrpc_server, "preflightBuyLandPrep",
+    "buy_land_prep");
+
+  function buy_land_prep($method_name, $params, $app_data)
+  {
+    global $dbhost, $dbuser, $dbpass, $dbname;
+
+    $confirmvalue = "";
+    $req = $params[0];
+    $agentid = $req['agentId'];
+    $sessionid = $req['secureSessionId'];
+    $amount = $req['currencyBuy'];
+    $billableArea = $req['billableArea'];
+
+    $ID = validate_user($agentid, $sessionid);
+
+    if($ID)
+    {
+      $membership_levels = array(
+        'levels' => array(
+        'id' => "00000000-0000-0000-0000-000000000000",
+        'description' => "some level"));
+
+      $landUse = array(
+        'upgrade' => False,
+        'action'  => "".SYSURL."");
+
+      $currency = array(
+        'estimatedCost' =>  "200.00");     // convert_to_real($amount));
+
+      $membership = array(
+        'upgrade' => False,
+        'action'  => "".SYSURL."",
+        'levels'  => $membership_levels);
+
+      $response_xml = xmlrpc_encode(array(
+         'success'    => True,
+         'currency'   => $currency,
+         'membership' => $membership,
+         'landUse'    => $landUse,
+         'currency'   => $currency,
+         'confirm'    => $confirmvalue));
+
+       header("Content-type: text/xml");
+       print $response_xml;
+    }
+    else
+    {
+      header("Content-type: text/xml");
+      $response_xml = xmlrpc_encode(array(
+        'success' => False,
+        'errorMessage' => "\n\nUnable to Authenticate\n\nClick URL for more info.",
+        'errorURI' => "".SYSURL.""));
+
+      print $response_xml;
+    }
+
+    return "";
+  }
+
+  $request_xml = $HTTP_RAW_POST_DATA;
+  xmlrpc_server_call_method($xmlrpc_server, $request_xml, '');
+  xmlrpc_server_destroy($xmlrpc_server);
+
+?>
